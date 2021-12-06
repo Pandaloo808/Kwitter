f5869b98c619dd15b64f3742d47ca72d6101318d
Pandaloo808 <93350605+Pandaloo808@users.noreply.github.com>
2021-12-06 16:43:46 -0500

    Update kwitter_page.js

--- kwitter_page.js
+++ kwitter_page.js
@@ -1,3 +1,15 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBrSjBmqHz_snlAMVegNgHyr95A54JkjoM",
+  authDomain: "doc-is-home.firebaseapp.com",
+  databaseURL: "https://doc-is-home-default-rtdb.firebaseio.com",
+  projectId: "doc-is-home",
+  storageBucket: "doc-is-home.appspot.com",
+  messagingSenderId: "1074863772693",
+  appId: "1:1074863772693:web:3a7eeb630fbaf989ddaa1c"
+};
+firebase.initializeApp(firebaseConfig);
+user_name=localStorage.getItem("user_name");
+room_name=localStorage.getItem("room_name")
 function send(){
   msg=document.getElementById("msg").value;
   firebase.database().ref(room_name).push({
@@ -5,4 +17,10 @@ function send(){
     message:msg,
     like:0
   });
-  document.getElementVyId("msg")
+  document.getElementVyId("msg").value="";
+}
+function logout(){
+  localStorage.removeItem("user_name");
+  localStorage.removeItem("room_name");
+  window.location="index.html"
+}
