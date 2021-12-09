9c84371ca1ee7aab7748f4d4a1e1dd696897fbb2
Pandaloo808 <93350605+Pandaloo808@users.noreply.github.com>
2021-12-09 16:49:21 -0500

    Update kwitter_page.js

--- kwitter_page.js
+++ kwitter_page.js
@@ -24,3 +24,33 @@ function logout(){
   localStorage.removeItem("room_name");
   window.location="index.html"
 }
+function getData(){
+   firebase.database().ref("/"+room_name).on('value', function(snapshot){ 
+   document.getElementById("output").innerHTML = ""; 
+   snapshot.forEach(function(childSnapshot){ 
+   childKey = childSnapshot.key; 
+   childData = childSnapshot.val(); 
+   if(childKey != "purpose"){ 
+   firebase_message_id = childKey; 
+   message_data = childData;
+     console.log(firebase_message_id);
+     console.log(message_data)
+     name=message_data['name'];
+     message=message_data['message'];
+     like=message_data['like'];
+     name_width_tag="<h4>"+name+"<img class='user_tick' src='https://thumbs.dreamstime.com/b/blue-tick-icon-vector-symbol-checkmark-isolated-white-background-checked-icon-correct-choice-sign-check-mark-blue-tick-icon-159108544.jpg'></h4>";
+     message_width_tag="<h4 class='message_h4'>"+message+"</h4>";
+     like_buton="button class='btn btn-warning' id="+firebase_message_id+"value="+like+"onlick='updateLike(this.id)'>";
+     span_width_tag="<span class='glyphicon glyphicon-thumbs-up'>like:"+like+"</span> </button> <hr>";
+     row=name_width_tag+message_width_tag+like_button+span_width_tag;
+     document.getElementById("output").innerHTML+=row;
+ } }); }); }
+getData();
+function updateLike(message_id){
+  console.log("clicked on like button"+message_id);
+  button_id=message_id;
+  likes=document.getElementById(button_id).value;
+  updated_likes=Number(likes)+1;
+  console.log(updated_likes);
+  firebase.database().ref(room_name).child(message_id).update({ like : updated_likes });
+}
